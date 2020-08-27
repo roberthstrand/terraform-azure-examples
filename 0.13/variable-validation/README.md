6225f0b72547a5a7e64204a063adae35ea990ace
Roberth Strand <me@robstr.dev>
2020-08-27 15:23:01 +0200

    Removed bad example, updated the README

--- 0.13/variable-validation/README.md
+++ 0.13/variable-validation/README.md
@@ -1,1 +1,27 @@
 # Variable Validation
+
+Custom variable validation was introduced as a production-ready feature from Terraform 0.13. With variable validation, you can predefine rules for what a input variable should allow and what kind of error it should report. By using variable validation, you can make sure that the input variable is correct at an earlier stage.
+
+## Storage account limitations
+
+```hcl
+# Create a storage account, making sure the name sticks to naming limitations.
+variable "storage_account_name" {
+  type = string
+  validation {
+    condition     = length(var.storage_account_name) > 2 && 
+                    length(var.storage_account_name) < 25 && 
+                    can(regex("[a-z.*]|[0-9]", var.storage_account_name
+                    ))
+    error_message = "Storage account names must be between 3 and 24 characters in length and may contain numbers and lowercase letters only."
+  }
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+```
--- 0.13/variable-validation/main.tf
+++ 0.13/variable-validation/main.tf
@@ -1,26 +1,13 @@
 # Variable Validation demonstration, Terraform 0.13
-# Note - Some of these might be a better fit for Azure Policy as this
-# is probably not the place to enforce policies. These are just examples.
-
-# Create a resource group, using the prefix rg- based on Azure naming convention
-variable "resource_group_name" {
-  type = string
-  validation {
-    condition     = substr(var.resource_group_name, 0, 3) == "rg-"
-    error_message = "Resource group names must start with with rg-, based on Microsoft Azure naming conventions."
-  }
-}
-
-resource "azurerm_resource_group" "demo" {
-  name     = var.resource_group_name
-  location = "Norway East"
-}
 
 # Create a storage account, making sure the name sticks to naming limitations.
 variable "storage_account_name" {
   type = string
   validation {
-    condition     = length(var.storage_account_name) > 2 && length(var.storage_account_name) < 25 && can(regex("[a-z.*]|[0-9]", var.storage_account_name))
+    condition     = length(var.storage_account_name) > 2 && 
+                    length(var.storage_account_name) < 25 && 
+                    can(regex("[a-z.*]|[0-9]", var.storage_account_name
+                    ))
     error_message = "Storage account names must be between 3 and 24 characters in length and may contain numbers and lowercase letters only."
   }
 }
